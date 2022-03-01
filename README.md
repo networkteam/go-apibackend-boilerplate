54bd0796086c701e0866193475c2944ce2805be4
Christopher Hlubek <christopher.hlubek@networkteam.com>
2022-03-01 11:36:43 +0100

    Add tmp and fix create script

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Go API backend boilerplate
 
+This is a boilerplate for creating a GraphQL API backend with go.
+It contains the setup for a monorepo application and is meant to be used as a starting point for creating a new project.
+Because every project is different, all files can be freely changed to reflect the actual needs of the project.
+E.g. it might use organisations for a multi-tenant application and authentication needs might differ.
+That's why a boilerplate can be better shaped and has fewer abstractions than using a modular framework.
+
 See [./docs](docs) for more information.
 
 
--- create.sh
+++ create.sh
@@ -14,20 +14,28 @@ mytld=$2
 myvendor=$3
 myproject=$4
 
-echo "Copy $DIR/backend to $target"
+echo "Copy $DIR to $target"
 
 mkdir -p $target
-# TODO Copy root files as well
-cp -r $DIR/backend $target/
+rsync -av $DIR/ $target/ --exclude backend/api/graph/generated --exclude .idea --exclude .git
 
 cd $target
+
+echo "Replacing placeholders"
 
 find . \( -type d -name .git -prune \) -o -type f -print0 | LC_ALL=C xargs -0 sed -i '' \
   "s/mytld/$mytld/g; s/myvendor/$myvendor/g; s/myproject/$myproject/g"
 
+pushd backend
+
 echo "Formatting Go code"
 
 go fmt ./...
+
+echo "Generating GraphQL API"
+go run github.com/99designs/gqlgen
+
+popd
 
 echo "Creating Git repository"
 
