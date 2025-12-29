c6a051ebde7795fa480361f1905e8e402623ec6c
Christopher Hlubek <christopher.hlubek@networkteam.com>
2025-12-29 18:24:35 +0100

    wip: add worker and process compose services

--- backend/persistence/repository/mappings.go
+++ backend/persistence/repository/mappings.go
@@ -1,3 +1,3 @@
 package repository
 
-//go:generate go run github.com/networkteam/construct/v2/cmd/construct myvendor.mytld/myproject/backend/domain
+//go:generate go run github.com/networkteam/construct/v2/cmd/construct myvendor.mytld/myproject/backend/domain/model
